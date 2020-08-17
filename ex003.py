515ba544ead86ab3b047f54b4b591f2627f3dc22
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2020-08-17 17:44:52 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois numeros e faça a soma entre eles! 
+
+a = int(input("Digite um numero: "))
+b = int(input("Digite um segundo numero: "))
+s = a + b
+print(f"A soma entre {a} e {b} resulta em {s}! ")
