88c4d50257ca1305e5328ced136b8df8fa3b1302
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2020-08-17 18:50:19 -0300

    Create ex036.py

---
+++ ex036.py
@@ -0,0 +1,4 @@
+''' Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. Pergunte o valor da casa, o salario
+do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salario ou então o emprestimo
+será negado '''
+
