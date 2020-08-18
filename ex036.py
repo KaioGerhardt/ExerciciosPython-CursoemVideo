4648ef0d6603d01e7c34f191e53de0dcb4eba464
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2020-08-18 20:04:16 -0300

    Update ex036.py

--- ex036.py
+++ ex036.py
@@ -2,3 +2,8 @@
 do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salario ou então o emprestimo
 será negado '''
 
+vcasa = float(input("Digite o valor da casa em R$ "))
+salario = float(input("Digite o seu salário em R$ "))
+anos = int(input("Digite em quantos anos pretende pagar a casa: "))
+prest = vcasa / (anos * 12)
+print(prest)
