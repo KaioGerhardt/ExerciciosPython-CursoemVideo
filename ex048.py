8a62ba9de1bc4a305d35292b0cabe37d5c32b6d0
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2020-08-18 16:08:18 -0300

    Update ex048.py

--- ex048.py
+++ ex048.py
@@ -3,13 +3,11 @@ no intervalo de 1 a 500 '''
 
 for c in range(1, 500, 2):
     c = []
+s = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont = cont + 1 # Poderia escreve desta forma "cont += 1", tanto para o contador como para o acumulador
+        s = s + c
 
-m = 3 % c
-if m == 0:
-    v = []
-
-s = v
-
-print(s)
-
-#Arrumar
+print(f"A soma de todos os {cont} valores solicitados é {s}!")
