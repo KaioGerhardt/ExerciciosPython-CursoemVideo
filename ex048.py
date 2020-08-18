b1a4bf3b9750c72559dcc0648ef0182d60569706
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2020-08-18 00:34:07 -0300

    Update ex048.py

---
+++ ex048.py
@@ -0,0 +1,13 @@
+''' Faça um programa que calcule a soma entre todos os numeros impares que são multiplos de tres e que se encontram
+no intervalo de 1 a 500 '''
+
+for c in range(1, 500, 2):
+    c = []
+
+m = 3 % c
+if m == 0:
+    v = []
+
+s = v
+
+print(s)
