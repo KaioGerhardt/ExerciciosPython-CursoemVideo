59677e60ddcb7f0d74a9b9e573f73d52c2867fac
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2020-08-18 20:04:09 -0300

    Create ex057.py

---
+++ ex057.py
@@ -0,0 +1,10 @@
+''' Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores M ou F. Caso esteja errado, peça a 
+digitação novamente até ter um valor correto. '''
+
+s = "F" or "M"
+while s:
+    s = str(input("Favor informe seu sexo: [F/M] ")).upper
+    if s != "M" or "F":
+        print("Favor insira uma resposta válida! ")
+    else:
+        print("Obrigado, resposta válida! ")
