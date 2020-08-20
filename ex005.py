87d27d914933d656ffb774276e62ed58765e0aef
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2020-08-19 23:21:39 -0300

    Create ex005.py

---
+++ ex005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um numero inteiro e mostrena tela o seu sucessor e antecessor.
+
+n = int(input("Digite um numero: "))
+s = n + 1
+a = n - 1
+print(f"O sucessor de {n} é {s} e o seu antecessor é {a}!")
