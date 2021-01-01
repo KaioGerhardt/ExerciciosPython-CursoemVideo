68f1083a9a0fab8b4cf9adf4030272ae4e3017e6
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2021-01-01 17:02:51 -0300

    Delete ex032.py

--- ex032.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que leia um ano qualquer e mostres se ele é bissexto.
-
-ano = int(input("Digite um ano para sabe se ele é bissexto: "))
-if ano % 4 == 0:
-    print(f"O ano de {ano} é BISSEXTO!")
-else:
-    print(f"O ano de {ano} NÂO é Bissexto!")
