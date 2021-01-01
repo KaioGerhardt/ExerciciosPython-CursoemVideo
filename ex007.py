008aa04f26f018158d9dfdc88f8de6bad690782f
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2021-01-01 16:59:45 -0300

    Delete ex007.py

--- ex007.py
+++
@@ -1,6 +0,0 @@
-# DEsenvolva um programa que leia duas notas de um aluno, calcule e mostre a sua média
-
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-m = (n1 + n2)/2
-print(f"A media entre as duas notas acima é {m:.1f}!")
