387c5bc8276d97adf2895e6e929287b333508f49
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2021-01-01 17:02:56 -0300

    Create ex006.py

---
+++ Mundo 1/ex006.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+import math
+ 
+num = float(input("Digite um numero: "))
+print (f"O dobro de {num} é {num * 2}, o triplo é {num * 3} e a sua raiz quadrada é {math.sqrt(num)}!")
