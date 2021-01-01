752ad5ad0256704f01a65f980ce8e9fa78213f96
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2021-01-01 16:59:34 -0300

    Delete ex004.py

--- ex004.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis 
-# sobre ela.
-
-a = input("Digite qualquer coisa: ")
-print("Qual é o seu tipo primitivo? ", type(a)) #Função utilizada para descobrir o tipo primitivo
-print("É apenas espaços? ", a.isspace()) # Utilizado para saber se possui apenas espaços 
-print("São letras e numeros? " a.isalnum())
