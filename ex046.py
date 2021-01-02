ae8cc8c31c3c59870ab6e64d20e3d36a18611d0b
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2021-01-02 11:01:04 -0300

    Delete ex046.py

--- ex046.py
+++
@@ -1,8 +0,0 @@
-''' Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artificios, indo de 10 até 0,
-com uma pausa de 1 segundo entre eles '''
-
-import time
-
-for c in range(10, -1, -1):
-    time.sleep(1)
-    print(c)
