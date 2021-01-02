8df74a217dfc8954a90473b872b9c2773b901a1f
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2021-01-02 11:01:12 -0300

    Create ex036.py

---
+++ Mundo 2/ex036.py
@@ -0,0 +1,14 @@
+''' Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. Pergunte o valor da casa, o salario
+do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salario ou então o emprestimo
+será negado '''
+
+vcasa = float(input("Digite o valor da casa em R$ "))
+salario = float(input("Digite o seu salário em R$ "))
+anos = int(input("Digite em quantos anos pretende pagar a casa: "))
+prest = vcasa / (anos * 12)
+
+if prest <= ((salario*30)/100):
+    print("Emprestimo APROVADO!")
+else:
+    print("Emprestimo NEGADO!")
+    
