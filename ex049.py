c38572e0d4f4264478c1d0f03948329f5555bc07
KaioGerhardt <KaioGerhardt@users.noreply.github.com>
2021-01-02 11:01:18 -0300

    Delete ex049.py

--- ex049.py
+++
@@ -1,7 +0,0 @@
-''' Refazer o mesmo desfio 009, mostrando a tabuada de um numero que o usuario escolher, só que agora utilizando
-o laço for. '''
-
-n = int(input("Digite um valor para saber sua tabuada: "))
-
-for c in range(1, 11):
-    print(f"{n} X {c} = {n*c}")
